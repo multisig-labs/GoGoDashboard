270ce282aa01ed58c0e9d64496787193547579f4
CarterLynch <carter@tiogaventures.com>
2022-06-22 19:09:27 -0700

    Added vars

--- src/components/TokenggAvax.js
+++ src/components/TokenggAvax.js
@@ -1,20 +1,20 @@
-import { formatEther,formatUnits } from "@ethersproject/units";
-import { useCall} from "@usedapp/core";
+import { formatEther, formatUnits } from "@ethersproject/units";
+import { useCall } from "@usedapp/core";
 import { Contract } from "@ethersproject/contracts";
 import { utils } from "ethers";
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 // Contract Address
-import contractAddresses from "../data/contractAddresses.json"
+import contractAddresses from "../data/contractAddresses.json";
 // ABI
-import TokenggAvaxABI from "../abi/contract/tokens/TokenggAVAX.sol/TokenggAVAX.json"
+import TokenggAvaxABI from "../abi/contract/tokens/TokenggAVAX.sol/TokenggAVAX.json";
 
 function useGGAVAXStats(func) {
   const TokenggAvaxInterface = new utils.Interface(TokenggAvaxABI.abi);
@@ -43,15 +43,19 @@ function TokenggAvax() {
   const lastRewardAmount = useGGAVAXStats("lastRewardAmount");
   const totalReleasedAssets = useGGAVAXStats("totalReleasedAssets");
   const stakingTotalAssets = useGGAVAXStats("stakingTotalAssets");
+  const targetFloatPercent = useGGAVAXStats("targetFloatPercent");
+  const totalFloat = useGGAVAXStats("totalFloat");
+  const amountAvailableForStaking = useGGAVAXStats("amountAvailableForStaking");
+  const totalAssets = useGGAVAXStats("totalAssets");
 
   return (
     <div>
-      <TableContainer sx={{ boxShadow:10}}component={Paper}>
+      <TableContainer sx={{ boxShadow: 10 }} component={Paper}>
         <Table sx={{ minWidth: 50 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Token ggAVAX</TableCell>
-              <TableCell align="right">Value</TableCell>
+              <TableCell><b>ggAVAX Variables:</b></TableCell>
+              <TableCell align="right"><b>Values:</b></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -67,8 +71,7 @@ function TokenggAvax() {
               <TableCell component="th" scope="row">
                 Last Sync
               </TableCell>
-                <TableCell align="right"> {lastSync}
-              </TableCell>
+              <TableCell align="right"> {lastSync}</TableCell>
             </TableRow>
             <TableRow>
               <TableCell component="th" scope="row">
@@ -83,7 +86,9 @@ function TokenggAvax() {
                 Last Reward Amount
               </TableCell>
               {lastRewardAmount && (
-                <TableCell align="right">{formatEther(lastRewardAmount)}</TableCell>
+                <TableCell align="right">
+                  {formatEther(lastRewardAmount)}
+                </TableCell>
               )}
             </TableRow>
             <TableRow>
@@ -91,54 +96,61 @@ function TokenggAvax() {
                 Total Released Assets
               </TableCell>
               {totalReleasedAssets && (
-                <TableCell align="right">{formatEther(totalReleasedAssets)}</TableCell>
+                <TableCell align="right">
+                  {formatEther(totalReleasedAssets)}
+                </TableCell>
               )}
             </TableRow>
             <TableRow>
               <TableCell component="th" scope="row">
-                StakingTotalAssets
+                Staking Total Assets
               </TableCell>
               {stakingTotalAssets && (
-                <TableCell align="right">{formatEther(stakingTotalAssets)}</TableCell>
+                <TableCell align="right">
+                  {formatEther(stakingTotalAssets)}
+                </TableCell>
+              )}
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Target Float Percent
+              </TableCell>
+              {targetFloatPercent && (
+                <TableCell align="right">
+                  {formatUnits(targetFloatPercent)}
+                </TableCell>
+              )}
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Total Float
+              </TableCell>
+              {totalFloat && (
+                <TableCell align="right">{formatUnits(totalFloat)}</TableCell>
+              )}
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Amount Available For Staking
+              </TableCell>
+              {amountAvailableForStaking && (
+                <TableCell align="right">
+                  {formatUnits(amountAvailableForStaking)}
+                </TableCell>
+              )}
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Total Assets
+              </TableCell>
+              {totalAssets && (
+                <TableCell align="right">{formatUnits(totalAssets)}</TableCell>
               )}
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     </div>
-
-    // <div>
-    //   <h3>ggAVAX:</h3>
-    //   <table>
-    //     <tr>
-    //       {totalFloat && (<><td>Total Float: </td><td>{formatEther(totalFloat)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {totalAssets && (<><td>Total Assets: </td><td>{formatEther(totalAssets)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {amountAvailableForStaking && (<><td>Available for Staking: </td><td>{formatEther(amountAvailableForStaking)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {stakingTotalAssets && (<><td>Staking Total Assets: </td><td>{formatEther(stakingTotalAssets)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {totalReleasedAssets && (<><td>Total Released Assets: </td><td>{formatEther(totalReleasedAssets)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {lastRewardAmount && (<><td>Last Reward Amount: </td><td>{formatEther(lastRewardAmount)}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {rewardsCycleEnd && (<><td>Rewards Cycle End </td><td>{rewardsCycleEnd}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {rewardsCycleLength && (<><td>Rewards Cycle Length: </td><td>{rewardsCycleLength}</td></>)}
-    //     </tr>
-    //     <tr>
-    //       {lastSync && (<><td>Last Sync: </td><td>{lastSync}</td></>)}
-    //     </tr>
-    //   </table>
-    // </div>
   );
 }
 
